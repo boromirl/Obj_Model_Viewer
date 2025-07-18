cb22721f0e4b692fc06d409fe072f344f85567b1
boromirl <burdiyanov@gmail.com>
2025-07-18 18:44:40 +0700

    parser tests

--- src/tests/test_parser.cc
+++ src/tests/test_parser.cc
@@ -17,3 +17,50 @@ TEST(SuiteParser, BasicStar) {
   ASSERT_EQ(modelData.vertices.size(), 10);
   ASSERT_EQ(modelData.edges.size(), 20);
 }
+
+TEST(SuiteParser, WrongFilePath) {
+  std::string filepath("wrong/filepath");
+  s21::ModelData modelData;
+
+  bool res = s21::ObjParser::Parse(filepath, &modelData);
+  ASSERT_FALSE(res);
+}
+
+TEST(SuiteParser, EmptyFile) {
+  std::string filepath("tests/test_assets/empty.obj");
+  s21::ModelData modelData;
+
+  bool res = s21::ObjParser::Parse(filepath, &modelData);
+
+  ASSERT_FALSE(res);
+  ASSERT_EQ(modelData.vertices.size(), 0);
+  ASSERT_EQ(modelData.edges.size(), 0);
+}
+
+TEST(SuiteParser, NegativeIndices) {
+  std::string filepath("tests/test_assets/negative_star.obj");
+  s21::ModelData modelData;
+
+  bool res = s21::ObjParser::Parse(filepath, &modelData);
+
+  // файл читается
+  ASSERT_TRUE(res);
+
+  // Правильное кол-во вершин и рёбер
+  ASSERT_EQ(modelData.vertices.size(), 10);
+  ASSERT_EQ(modelData.edges.size(), 20);
+}
+
+TEST(SuiteParser, BiggerFile) {
+  std::string filepath("tests/test_assets/lowpoly_tree.obj");
+  s21::ModelData modelData;
+
+  bool res = s21::ObjParser::Parse(filepath, &modelData);
+
+  // файл читается
+  ASSERT_TRUE(res);
+
+  // Правильное кол-во вершин и рёбер
+  ASSERT_EQ(modelData.vertices.size(), 281);
+  ASSERT_EQ(modelData.edges.size(), 581);
+}
